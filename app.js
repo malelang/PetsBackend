e70eaf8e52335050c74404d63f845aecd3a9c7bb
malelang <landiezm@unicauca.edu.co>
2017-10-09 17:00:06 -0500

    Solucionado Postman

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb').MongoClient;
+var cors = require('cors');
+
+var users = require('./routes/users');
+var pets = require('./routes/pets');
+
+var app = express();
+app.use(cors());
+let dbMongo;
+mongodb.connect("mongodb://localhost:27017/mascotas",(err,db)=>{
+    if(err){
+        console.log("Error al conectarse a mongo")
+    }
+    dbMongo=db;
+});
+//TODOS ESTOS SON MIDDLEWARES
+app.use((req,res,next)=>{
+    req.db=dbMongo;
+    next(); 
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/pets', pets);
+app.use('/api/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  // error handlers
+  
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+  
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
+  
+
+module.exports = app;
---
+++ routes/pets.js
@@ -0,0 +1,59 @@
+var express =require('express');
+var router = express.Router();
+var ObjectID=require('mongodb').ObjectID;
+
+router.use((req,res,next)=>{
+    req.collection = req.db.collection('pets');
+    next();
+})
+
+router.get("/", (req, res, next) => {
+    req.collection.find().toArray().then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.send([]);
+    });
+});
+
+
+router.get("/:id",(req,res,next)=>{
+    let id= req.params.id;
+    req.collection.findOne({_id: new ObjectID(id)})
+    .then(result=>{
+        if(result){
+            res.send(result);
+        }else{
+            res.status(404).send({err:"Mascota no encontrada"})
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({err:"Error al leer libros "})
+    });
+});
+
+router.post("/",(req,res,next)=>{
+    let body=req.body; // asi obtengo la info que viaja a traves del método post
+    req.collection.insertOne(body)
+    .then(result=>{
+        res.send({success: true})
+    }).catch(err=>{
+        res.send({success:false})
+    })
+});
+
+router.put("/:id",(req,res,next)=>{
+    let body=req.body;
+    let id= new ObjectID(req.params.id)
+    req.collection.updateOne({_id:id},{$set:body})
+    .then(result=>res.send({success:true}))
+    .catch(err=>res.send({success:false}));
+});
+
+router.delete("/:id",(req,res,next)=>{
+    let id=new ObjectID(req.params.id);
+    req.collection.deleteOne({_id:id})
+    .then(result=>res.send({success:true}))
+    .catch(err=>res.send({success:false}))
+});
+
+module.exports =router;
