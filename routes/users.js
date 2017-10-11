120866bb5f1f4444e31dc48cb1266043b2a8953a
malelang <landiezm@unicauca.edu.co>
2017-10-10 22:31:22 -0500

    Users implmentado

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
-var express = require('express');
+var express =require('express');
 var router = express.Router();
+var ObjectID=require('mongodb').ObjectID;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+router.use((req,res,next)=>{
+    req.collection = req.db.collection('users');
+    next();
+})
+
+router.get("/", (req, res, next) => {
+    req.collection.find().toArray().then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.send([]);
+    });
 });
 
-module.exports = router;
+
+router.get("/:id",(req,res,next)=>{
+    let id= req.params.id;
+    req.collection.findOne({_id: new ObjectID(id)})
+    .then(result=>{
+        if(result){
+            res.send(result);
+        }else{
+            res.status(404).send({err:"Usuario no encontrado"})
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({err:"Error al leer usuarios "})
+    });
+});
+
+router.post("/",(req,res,next)=>{
+    let body=req.body; // asi obtengo la info que viaja a traves del método post
+    req.collection.insertOne(body)
+    .then(result=>{
+        res.send({success: true})
+    }).catch(err=>{
+        res.send({success:false})
+    })
+});
+
+router.put("/:id",(req,res,next)=>{
+    let body=req.body;
+    let id= new ObjectID(req.params.id)
+    req.collection.updateOne({_id:id},{$set:body})
+    .then(result=>res.send({success:true}))
+    .catch(err=>res.send({success:false}));
+});
+
+router.delete("/:id",(req,res,next)=>{
+    let id=new ObjectID(req.params.id);
+    req.collection.deleteOne({_id:id})
+    .then(result=>res.send({success:true}))
+    .catch(err=>res.send({success:false}))
+});
+
+module.exports =router;
